070bff2eb3f5b1807d2013979fa51f169b44850e
Lohannes da Silva Costa <lohannes.s26@gmail.com>
2022-09-01 16:58:44 -0300

    tela

--- src/br/senai/sp/jandira/gui/FrameTabuada.java
+++ src/br/senai/sp/jandira/gui/FrameTabuada.java
@@ -1,5 +1,42 @@
 package br.senai.sp.jandira.gui;
 
+import java.awt.Color;
+import java.awt.Container;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class FrameTabuada {
 
+	//atributos da Tela
+	public String titulo;
+	public int altura;
+	public int largura;
+	
+	//Criação da tela
+	public void criarTela() {
+		
+		JFrame tela = new JFrame();
+		tela.setTitle(titulo);
+		tela.setSize(largura, altura);
+		tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		tela.setLayout(null);
+		
+		//Criação do container
+		Container painel =  tela.getContentPane();
+		
+		//Criação dos componentes da tela
+		JLabel tituloTabuada = new JLabel();
+		tituloTabuada.setText("Tabuada");
+		tituloTabuada.setBounds(70, 0, 100, 50);
+		
+		JLabel descricaoTabuada = new JLabel();
+		tituloTabuada.setText("Tabuada");
+		tituloTabuada.setBounds(70, 0, 100, 50);
+		
+		painel.add(tituloTabuada);
+		tela.setVisible(true);
+	}
+	
 }
--- src/br/senai/sp/jandira/model/Tabuada.java
+++ src/br/senai/sp/jandira/model/Tabuada.java
@@ -1,5 +1,22 @@
 package br.senai.sp.jandira.model;
 
-public class Tabuada {
+import java.util.ArrayList;
 
+public class Tabuada {
+		
+	public int numeroMultiplicado;
+	public int minMultiplicador;
+	public int maxMultiplicador;
+	public ArrayList<String> conta = new ArrayList<String>();
+	public int resultado = numeroMultiplicado * minMultiplicador;
+	
+	public void getTabuada() {
+		
+		while (minMultiplicador < maxMultiplicador) {
+			conta.add(numeroMultiplicado + " x " + minMultiplicador + " = " + resultado);
+			minMultiplicador++;
+		}
+		
+	}
+	
 }
